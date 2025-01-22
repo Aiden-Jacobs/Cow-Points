ed368556c444d0d1903362f6ae4cb450783a40ea
Aiden-Jacobs <aiden192@gmail.com>
2025-01-22 15:30:42 -0700

    Add user navigation bar component and custom font; refactor dashboard

---
+++ src/components/user_nav_bar.js
@@ -0,0 +1,18 @@
+class User_nav_bar extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+        this.innerHTML = `<nav>
+                    <ul id="nav-bar">
+                        <li><a href="user_dashboard.html">Dashboard</a></li>
+                        <li><a href="points.html">Points</a></li>
+                        <li><a href="friends.html">Friends</a></li>
+                        <li><a href="#add-point">Add Point</a></li>
+                        <li><a href="user_settings.html">Settings</a></li>
+                    </ul>
+                </nav>`;
+    }
+  }
+
+customElements.define('user-nav-bar', User_nav_bar);
