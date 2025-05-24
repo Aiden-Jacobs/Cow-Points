6f8048aeff2115e5a805e534fd391b2aeaa4eb81
Aiden-Jacobs <aiden192@gmail.com>
2025-05-23 18:35:02 -0700

    Uncomment 'Friends' link in user navigation bar

--- src/components/user_nav_bar.js
+++ src/components/user_nav_bar.js
@@ -7,7 +7,7 @@ class User_nav_bar extends HTMLElement {
                     <ul id="nav-bar">
                         <li><a href="user_dashboard.html">Dashboard</a></li>
                         <li><a href="points.html">Points</a></li>
-                        <!-- <li><a href="friends.html">Friends</a></li> -->
+                        <li><a href="friends.html">Friends</a></li>
                         <!-- <li><a href="#add-point">Add Point</a></li> -->
                         <!-- <li><a href="user_settings.html">Settings</a></li> -->
                     </ul>
